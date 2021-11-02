316f635c461f06bbb6e557118ad53b39a8a79f43
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-11-02 16:28:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import numpy as np
 from skimage import io
 from sklearn.neighbors import KNeighborsClassifier
 import gc
+import cv2
 
 # Directories
 source_dir = os.getcwd() # current working directory
@@ -27,7 +28,7 @@ x_train = []
 for case in os.listdir(os.path.join(dataset_dir, 'train')):
     for image in os.listdir(os.path.join(dataset_dir, 'train', case)):
             if image.endswith(".jpg") and not image.startswith("."):
-                pseudo_x = io.imread(os.path.join(dataset_dir, 'train', case, image), as_gray = False)
+                pseudo_x = cv2.imread(os.path.join(dataset_dir, 'train', case, image))
                 x_train.append(pseudo_x)
                 y_train.append(case)
     
@@ -37,35 +38,41 @@ x_val = []
 for case in os.listdir(os.path.join(dataset_dir, 'val')):
     for image in os.listdir(os.path.join(dataset_dir, 'val', case)):
             if image.endswith(".jpg") and not image.startswith("."):
-                pseudo_x = io.imread(os.path.join(dataset_dir, 'val', case, image), as_gray = False)
+                pseudo_x = cv2.imread(os.path.join(dataset_dir, 'val', case, image))
                 x_val.append(pseudo_x)
                 y_val.append(case)
 
 del pseudo_x              
 gc.collect()
     
-#%% Preprocessing
+#%% Preprocessing & Feature Extraction
+### RBG ###
 from skimage.color import rgb2hsv
-from itertools import chain
 
 # List to array
-x_train_arr = np.array(x_train, dtype = np.float16)
+x_train_arr = np.array(x_train, dtype = np.float16())
 x_val_arr = np.array(x_val, dtype = np.float16)
 
 del x_train
 del x_val
 gc.collect()
 
-# Color Space Transformation: RGB --> HSV
+# SIFT
+
+
+sift = cv2.SIFT()
+kp, des = sift.detectAndCompute(x_train_arr[100,:,:,:],None)
+
+### Color Space Transformation: RGB --> HSV ###
 Y_val = []
 Y_train = []
 
 for i in range(0,x_train_arr.shape[0]):
-    x_train_arr[i] = rgb2hsv(x_train_arr[i])
+    # x_train_arr[i] = rgb2hsv(x_train_arr[i])
     Y_train.append(y_train[i])
     
 for i in range(0,x_val_arr.shape[0]):
-    x_val_arr[i] = rgb2hsv(x_val_arr[i])
+    # x_val_arr[i] = rgb2hsv(x_val_arr[i])
     Y_val.append(y_val[i])
 
 del y_train
@@ -74,39 +81,36 @@ gc.collect()
 
 #%% Feature Extraction
 
-feature_vector_train = np.zeros(x_train_arr.shape[0])
-feature_vector_val = np.zeros(x_val_arr.shape[0])
+mean_of_train = np.zeros(x_train_arr.shape[0])
+mean_of_val = np.zeros(x_val_arr.shape[0])
 
 # Mean of image
 for i in range(1,x_train_arr.shape[0]):
-    feature_vector_train[i] = np.mean(x_train_arr[i,:,:,2])
+    mean_of_train[i] = np.mean(x_train_arr[i,:,:,2])
     
 for i in range(1,x_val_arr.shape[0]):
-    feature_vector_val[i] = np.mean(x_val_arr[i,:,:,2])
+    mean_of_val[i] = np.mean(x_val_arr[i,:,:,2])
     
-# SIFT
+
 
 # LBP
 
-del x_train_arr
-del x_val_arr
-gc.collect()
+# del x_train_arr
+# del x_val_arr
+# gc.collect()
 
-#%% Inputs
+# #%% Inputs
 
-X_train = feature_vector_train[np.newaxis].T
-labels_train = np.where(np.array(Y_train) == 'les',1,0)
+# X_train = feature_vector_train[np.newaxis].T
+# Y_train = np.where(np.array(Y_train) == 'les',1,0)
 
 
-X_val = feature_vector_val[np.newaxis].T
-labels_val = np.where(np.array(Y_val) == 'les',1,0)
+# X_val = feature_vector_val[np.newaxis].T
+# Y_val = np.where(np.array(Y_val) == 'les',1,0)
 
-del feature_vector_train
-del feature_vector_val
-
-del Y_train
-del Y_val
-gc.collect()
+# del feature_vector_train
+# del feature_vector_val
+# gc.collect()
 
 #%%%%%%%%%%%%%%%%%%%%%%% Feature Selection %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -156,12 +160,12 @@ gc.collect()
 
 #%%%%%%%%%%%%%%%%%%% CLASSIFICATION %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-neigh = KNeighborsClassifier(n_neighbors=2)
-neigh.fit(X_train,labels_train)
+# neigh = KNeighborsClassifier(n_neighbors=2)
+# neigh.fit(X_train,labels_train)
     
-# Predictions
-print(neigh.predict([[1]])) # hard classification prediction
-print(neigh.predict_proba([[0.9]])) # confidence score prediction
+# # Predictions
+# print(neigh.predict([[1]])) # hard classification prediction
+# print(neigh.predict_proba([[0.9]])) # confidence score prediction
 
 
 
