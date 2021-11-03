6e1b411835b6851fb928411463b66a09b7f40265
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-11-03 21:30:28 +0100

    up

--- hairRemoval.py
+++ hairRemoval.py
@@ -8,10 +8,11 @@ def hairRemoval(img):
     inpaint_img = cv2.inpaint(img, black_hat, 7, flags=cv2.INPAINT_TELEA)
     return inpaint_img
 
-img_path = 'F:\\DISCO_DURO\\Mixto\\Subjects\\GitHub\\dataset2\\train\\bcc\\bcc0065.jpg'
-img = cv2.imread(img_path)
-cv2.imshow('original', img)
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img2 = hairRemoval(img_gray)
-cv2.imshow('processed', img2)
-cv2.waitKey()
+if __name__ == '__main__':   
+    img_path = 'F:\\DISCO_DURO\\Mixto\\Subjects\\GitHub\\dataset2\\train\\bcc\\bcc0065.jpg'
+    img = cv2.imread(img_path)
+    cv2.imshow('original', img)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img2 = hairRemoval(img_gray)
+    cv2.imshow('processed', img2)
+    cv2.waitKey()
