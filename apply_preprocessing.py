1b73ccbc2658d784c5b74a80f6b048d8b0c25e41
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-11-07 15:52:15 +0100

    Create apply_preprocessing.py

---
+++ apply_preprocessing.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 09:34:02 2021
+
+@author: Manuel Ojeda & Alexandru Vasile
+"""
+
+import tensorflow as tf
+import os
+import numpy as np
+from skimage import io
+from sklearn.neighbors import KNeighborsClassifier
+import gc
+import cv2
+from libb import preprocessing
+
+# Directories
+source_dir = os.getcwd() # current working directory
+project_dir = os.path.dirname(source_dir) # where the dataset folder should be
+dataset_dir = "D:\\Uni\\Spain\\CADx\\test"
+
+# Generator Parameters
+img_size = (450, 600,3) # RGB imges!
+batch_size = 32
+
+
+# Getting test images
+y_train = []
+x_train = []
+for case in os.listdir(os.path.join(dataset_dir)):
+        if case.endswith(".jpg") and not case.startswith("."):
+            pseudo_x = cv2.imread(os.path.join(dataset_dir, case))
+            x_train.append(pseudo_x)
+            y_train.append(case)
+
+del pseudo_x              
+gc.collect()
+    
+#%% Preprocessing & Feature Extraction
+# List to array
+x_train_arr = np.array(x_train)
+
+gc.collect()
+
+### Color Space Transformation: RGB --> HSV ###
+Y_val = []
+Y_train = []
+mean_of_train_hue = np.zeros(x_train_arr.shape[0], dtype = np.float32)[np.newaxis].T
+mean_of_train_sat = np.zeros(x_train_arr.shape[0], dtype = np.float32)[np.newaxis].T
+mean_of_train_val = np.zeros(x_train_arr.shape[0], dtype = np.float32)[np.newaxis].T
+
+mean_of_val_hue = np.zeros(x_train_arr.shape[0], dtype = np.float32)[np.newaxis].T
+mean_of_val_sat = np.zeros(x_train_arr.shape[0], dtype = np.float32)[np.newaxis].T
+mean_of_val_val = np.zeros(x_train_arr.shape[0], dtype = np.float32)[np.newaxis].T
+
+
+for i in range(0,x_train_arr.shape[0]):
+    x_train_arr[i] = cv2.cvtColor(x_train_arr[i],cv2.COLOR_RGB2HSV)
+    x_train_arr[i] = preprocessing(x_train_arr[i])
+    Y_train.append(y_train[i])
+    
+    filename_train = 'D:\\Uni\\Spain\\CADx\\preprocessing_test\\{}_{}.jpg'.format(y_train[i],i)
+    cv2.imwrite(filename_train,x_train_arr[i,:,:,2])
+    
+    mean_of_train_hue[i] = np.mean(x_train_arr[i,:,:,0]) # getting the mean of the hue channel
+    mean_of_train_sat[i] = np.mean(x_train_arr[i,:,:,1]) # getting the mean of the hue channel
+    mean_of_train_val[i] = np.mean(x_train_arr[i,:,:,2]) # getting the mean of the hue channel
+
+
+mean_of_train = np.concatenate((mean_of_train_hue, mean_of_train_sat, mean_of_train_val), axis=1)
+mean_of_val = np.concatenate((mean_of_val_hue, mean_of_val_sat, mean_of_val_val), axis=1)
+# save to csv file
+np.savetxt('mean_hsv_train.csv', mean_of_train, delimiter=',')
+np.savetxt('mean_hsv_val.csv', mean_of_val, delimiter=',')
+
+# Test reading
+load_hsv_train = np.loadtxt('mean_hsv_train.csv', dtype=np.float64, delimiter=',')[0:1200]
+load_hsv_val = np.loadtxt('mean_hsv_val.csv', dtype=np.float64, delimiter=',')[0:1200]
+
+del y_train
+gc.collect()
+
+#%% Loading the images 
+x_tr_load = []
+dataset_dir = os.path.join(project_dir, 'preprocessing') 
+for case in os.listdir(os.path.join(dataset_dir, 'train')):
+    if image.endswith(".jpg") and not image.startswith("."): # look here for muliclass labeling
+        # if (aux%100==0):
+        x_tr_load.append(cv2.imread(os.path.join(dataset_dir, 'train', case), cv2.IMREAD_GRAYSCALE))
+        
+x_load_array = np.array(x_tr_load,dtype = np.uint8)
+        
+#%% Fine Segmentation
+# from skimage.morphology import area_opening
+# x_l_mask = np.array(x_load_array > 0, dtype = np.uint8)
+# x_up_mask = np.array(x_load_array < 200 , dtype = np.uint8)
+# x_mask = np.multiply(x_l_mask, x_up_mask)
+
+# x_mask_open = area_opening(np.uint8(x_mask))
+
+# # for i in range(0,x_val_arr.shape[0]):
+
+# x_load_array = np.multiply(x_mask, x_load_array)
+
+                
+
+
+
+
