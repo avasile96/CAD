6cf0c3ee5139a1f82a9bb5917873e1c9556d1d5f
Manuel-O <74353087+Manuel-O@users.noreply.github.com>
2021-11-08 13:26:19 -0600

    up

--- main_ML.py
+++ main_ML.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 """
 Created on Sun Nov  7 05:55:10 2021
 
@@ -11,6 +11,7 @@ from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.metrics import accuracy_score
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.preprocessing import StandardScaler
+import pickle
 
 # Directories
 source_dir = os.getcwd() # current working directory
@@ -50,11 +51,13 @@ nH_lbp_train = np.loadtxt(nH_lbp_train_path, dtype=np.float32, delimiter=',')
 nH_lbp_val = np.loadtxt(nH_lbp_val_path, dtype=np.float32, delimiter=',')
 
 # Concatenate all the files to get x_train and x_val
-x_train = np.concatenate((mean_hue_train, nH_gb_lbp_train, gb_lbp_train,
-                          nH_lbp_train), axis=1)
-x_val = np.concatenate((mean_hue_val, nH_gb_lbp_val, gb_lbp_val,
-                        nH_lbp_val), axis=1)
+# mean_hue_train = mean_hue_train[:,0][np.newaxis].T
+# x_train = np.concatenate((mean_hue_train, nH_gb_lbp_train, gb_lbp_train), axis=1)
+# mean_hue_val = mean_hue_val[:,0][np.newaxis].T
+# x_val = np.concatenate((mean_hue_val, nH_gb_lbp_val, gb_lbp_val), axis=1)
 
+x_train = np.concatenate((mean_hue_train, nH_gb_lbp_train), axis=1)
+x_val = np.concatenate((mean_hue_val, nH_gb_lbp_val), axis=1)
 # save to csv file, WHEN FINAL FILE IS READY
 # np.savetxt(csvfile_train_path, x_train, delimiter=',')
 # np.savetxt(csvfile_val_path, x_val, delimiter=',')
@@ -81,28 +84,29 @@ from imblearn.under_sampling import RandomUnderSampler
 
 # autopct = "%.2f"
 
-sampling_strategy = "not minority"
+# sampling_strategy = "not minority"
 # fig, axs = plt.subplots(ncols=2, figsize=(10, 5))
-rus = RandomUnderSampler(sampling_strategy=sampling_strategy)
-X_res, y_res = rus.fit_resample(x_train, y_train)
-y_res = pd.DataFrame(y_res)
+# rus = RandomUnderSampler(sampling_strategy=sampling_strategy)
+# X_res, y_res = rus.fit_resample(x_train, y_train)
+# y_res = pd.DataFrame(y_res)
 # y_res.value_counts().plot.pie(autopct=autopct, ax=axs[0])
 # axs[0].set_title("Under-sampling")
 
 # sampling_strategy = "not majority"
 # ros = RandomOverSampler(sampling_strategy=sampling_strategy)
 # X_res, y_res = ros.fit_resample(x_train, y_train)
-y_res = pd.DataFrame(y_res)
+# y_res = pd.DataFrame(y_res)
 # y_res.value_counts().plot.pie(autopct=autopct, ax=axs[1])
 # axs[1].set_title("Over-sampling")
 
-x_train = X_res
-y_train = y_res.to_numpy()
+# x_train = X_res
+# y_train = y_res.to_numpy()
 
 
 #%% Feature selection
-
-param_kbest = SelectKBest(f_classif, k=2000)
+k = 1500
+print('k: ', k)
+param_kbest = SelectKBest(f_classif, k=k)
 param_kbest.fit(x_train, y_train)
 x_train_kbest = param_kbest.transform(x_train)  # Then we transform both the training an the test set
 x_test_kbest = param_kbest.transform(x_val)
@@ -155,22 +159,22 @@ print('Naive Bayes classifier: ', acc_NB)
 from sklearn.svm import SVC
 
 parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
-                           'C': [0.01, 1, 3, 10, 100, 1000]},
-                          {'kernel': ['poly'], 'C': [0.01, 3, 10, 100, 1000]},
-                          {'kernel': ['sigmoid'], 'C': [0.01, 3, 10, 100, 1000]}
+                           'C': [0.01, 1, 3, 10, 100, 300]},
+                          {'kernel': ['poly'], 'C': [0.01, 3, 10, 100, 300]},
+                          {'kernel': ['sigmoid'], 'C': [0.01, 3, 10, 100, 300]}
                           ]
 svc = SVC()
 clf = GridSearchCV(svc, parameters, cv=cv, refit=True)
 clf.fit(x_train, y_train)
 
 params_best_SVC = clf.best_params_
-print('Best parameters for SVC with DD for are = ', params_best_SVC)
+print('Best parameters for SVC are = ', params_best_SVC)
 
 y_pred_svc = clf.predict(x_val)
  
 # model accuracy for X_test 
 acc_SVC = accuracy_score(y_val, y_pred_svc)
-print("Accuracy SVC with DD is = ", acc_SVC)
+print("Accuracy SVC is = ", acc_SVC)
 
 
 #%% Decission tree classifier
@@ -179,18 +183,18 @@ from sklearn.tree import DecisionTreeClassifier
 dtc = DecisionTreeClassifier()
 param_grid_dt = {
     'max_features': ['auto', 'sqrt', 'log2'],
-    'max_depth': [2, 8, 10, 15, 20, 35, 50],
+    'max_depth': [2, 8, 10, 15, 20, 35],
     'criterion': ['gini', 'entropy']
 }
 
 GS_dtc = GridSearchCV(estimator=dtc, param_grid=param_grid_dt, cv=cv, refit=True)
 GS_dtc.fit(x_train, y_train)
 params_best_dt = GS_dtc.best_params_
-print('Best parameters for Random forest are = ', params_best_dt)
+print('Best parameters for Decission tree are = ', params_best_dt)
 
 y_pred = GS_dtc.predict(x_val)
 acc_DT = accuracy_score(y_val, y_pred)
-print("Accuracy Random forest is = ", acc_DT)
+print("Accuracy Decission tree is = ", acc_DT)
 
 
 #%% Random Forest
@@ -198,10 +202,8 @@ from sklearn.ensemble import RandomForestClassifier
 
 rfc = RandomForestClassifier(random_state=42)
 param_grid_rf = {
-    'n_estimators': [30, 100, 250, 1000],
-    'max_features': ['auto', 'sqrt', 'log2'],
-    'max_depth': [2, 8, 10, 15, 20, 35, 50],
-    'criterion': ['gini', 'entropy']
+    'n_estimators': [30, 100, 250, 400],
+    'max_depth': [2, 8, 10, 15, 20, 35]
 }
 
 GS_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid_rf, cv=cv, refit=True)
@@ -219,10 +221,8 @@ from sklearn.ensemble import ExtraTreesClassifier
 
 etc = ExtraTreesClassifier(random_state=42)
 param_grid_et = {
-    'n_estimators': [30, 100, 250, 1000],
-    'max_features': ['auto', 'sqrt', 'log2'],
-    'max_depth': [2, 8, 10, 15, 20, 35, 50],
-    'criterion': ['gini', 'entropy']
+    'n_estimators': [30, 100, 250, 400],
+    'max_depth': [2, 8, 10, 15, 20, 35]
 }
 
 GS_etc = GridSearchCV(estimator=etc, param_grid=param_grid_et, cv=cv, refit=True)
@@ -235,24 +235,42 @@ acc_ET = accuracy_score(y_val, y_pred)
 print("Accuracy Extra trees is = ", acc_ET)
 
 
-#%% Gradient Boosting Classifier
-from sklearn.ensemble import GradientBoostingClassifier
+#%% One VS Rest Classifier
+from sklearn.multiclass import OneVsRestClassifier
 
-gbc = GradientBoostingClassifier(random_state=29)
-param_grid_gb = {
-    'criterion': ['deviance', 'exponential'],
-    'learning_rate': [0.1, 0.01, 0.001, 0.3, 0.003, 0.003],
-    'max_features': ['auto', 'sqrt', 'log2'],
-    'max_depth': [3, 8, 10, 15, 20, 35, 50],
-}
+ovr = OneVsRestClassifier(SVC())
+ovr.fit(x_train, y_train)
 
-GS_gbc = GridSearchCV(estimator=gbc, param_grid=param_grid_gb, cv=cv, refit=True)
-GS_gbc.fit(x_train, y_train)
-params_best_GB = GS_gbc.best_params_
-print('Best parameters for Gradient boosting are = ', params_best_GB)
-
-y_pred = GS_gbc.predict(x_val)
-acc_GB = accuracy_score(y_val, y_pred)
-print("Accuracy Gradient boosting is = ", acc_GB)
+y_pred = ovr.predict(x_val)
+acc_OVR = accuracy_score(y_val, y_pred)
+print("Accuracy OneVsRest Classifier is = ", acc_OVR)
 
 
+#%% Save ML model, selectKBest, StandardScaler
+
+# save the model to disk
+# filename = 'finalized_GS_etc_model_614.sav'
+# pickle.dump(GS_etc, open(filename, 'wb'))
+ 
+# # load the model from disk
+# loaded_model = pickle.load(open(filename, 'rb'))
+# result = loaded_model.score(x_val, y_val)
+# print(result)
+
+# # save the selectKBest to disk
+# filename_kbest = 'finalized_kbest_model_600_etc614.sav'
+# pickle.dump(param_kbest, open(filename_kbest, 'wb'))
+ 
+# # load the selectKBest from disk
+# loaded_model_kbest = pickle.load(open(filename_kbest, 'rb'))
+# n_features_in = loaded_model_kbest.n_features_in_
+# print(n_features_in)
+
+# # save the StandardScaler to disk
+# filename_scaler = 'finalized_scaler_model_600_etc614.sav'
+# pickle.dump(scaler, open(filename_scaler, 'wb'))
+ 
+# load the StandardScaler from disk
+# loaded_model_scaler = pickle.load(open(filename_kbest, 'rb'))
+# n_features_in = loaded_model_scaler.n_features_in_
+# print(n_features_in)
