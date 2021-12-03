0cab56a318df7be4aa40bd0df8dbabde1ae25252
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-12-03 22:11:35 +0100

    Update main_DL.py

--- main_DL.py
+++ main_DL.py
@@ -21,6 +21,7 @@ from tensorflow.keras.models import Model, Sequential
 
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
+import random
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
@@ -32,9 +33,9 @@ project_dir = os.path.dirname(source_dir) # where the dataset folder should be
 dataset_dir = os.path.join(project_dir, 'dataset') 
 
 # Generator Parameters
-img_size = (450, 600,3) # RGB imges!
+img_size = (450, 600, 3) # RGB imges!
 # img_size = (225, 300,3) # RGB imges!
-batch_size = 16
+batch_size = 8
 
 # Getting paths to images
 train_img_paths = []
@@ -44,10 +45,8 @@ aux = 0
 for case in os.listdir(os.path.join(dataset_dir, 'train')):
     for image in os.listdir(os.path.join(dataset_dir, 'train', case)):
             if image.endswith(".jpg") and not image.startswith("."):
-                if (aux%5==0):
+                if (aux%10==0):
                     train_img_paths.append(os.path.join(dataset_dir, 'train', case, image)) # for DL
-                    # pseudo_x = io.imread(os.path.join(dataset_dir, 'train', case, image), as_gray = False)
-                    # x.append(pseudo_x)
                     img_label.append(case)
                 aux+=1
     
@@ -57,8 +56,8 @@ aux = 0
 for case in os.listdir(os.path.join(dataset_dir, 'val')):
     for image in os.listdir(os.path.join(dataset_dir, 'val', case)):
             if image.endswith(".jpg") and not image.startswith("."):
-                if (aux%5==0):
-                    val_img_paths.append(os.path.join(dataset_dir, case, image))
+                if (aux%10==0):
+                    val_img_paths.append(os.path.join(dataset_dir, 'val', case, image)) # for DL
                     val_label.append(case)
                 aux+=1
 
@@ -73,37 +72,32 @@ class SkinImageDatabase(tf.keras.utils.Sequence):
         self.batch_size = batch_size
         self.img_size = img_size
         self.input_img_paths = input_img_paths
+        random.shuffle(self.input_img_paths)
         self.img_label = img_label
+        self.shuffle = True
 
     def __len__(self):
         return len(self.input_img_paths) // self.batch_size
 
     def __getitem__(self, idx):
-        """Returns tuple (input, target) correspond to batch #idx."""
         i = idx * self.batch_size
         batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
-        batch_target_img_labels = self.img_label[i : i + self.batch_size]
 
         x = np.zeros((self.batch_size,) + self.img_size, dtype="uint8")
+        y = np.zeros((self.batch_size,) + (1,), dtype='uint8')
         for j, path in enumerate(batch_input_img_paths):
+            # img = io.imread(path, as_gray = False)
             img = io.imread(path, as_gray = False)
             # img = resize(img, (img.shape[0] // 2, img.shape[1] // 2),
                        # anti_aliasing=True)
             x[j] = img
-
-        y = np.zeros((self.batch_size,) + (1,), dtype='uint8')
-        for j, path in enumerate(batch_target_img_labels):
             
-            # y[j] = path
-            if path == 'les':
+            if path[-10:-8] == 'ls':
                 y[j] = 1
             else:
                 y[j] = 0
-        
-        # y = np.where(y == 'les', 1, 0)
-        
-        return x, y
-
+                
+        return x, tf.keras.utils.to_categorical(y, num_classes=2)
 
 traingen = SkinImageDatabase(batch_size, img_size, train_img_paths, img_label)
 valgen = SkinImageDatabase(batch_size, img_size, val_img_paths, val_label)
@@ -131,30 +125,35 @@ model = Sequential()
 # Add the vgg convolutional base model
 model.add(vgg19)
 # Add new layers
+# model.add(Dropout(0.2))
 model.add(Flatten())
-model.add(Dense(10, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1)) #TODO
+# model.add(Dense(10, activation='relu'))
+# model.add(Dropout(0.2))
+model.add(Dense(2, activation='softmax'))
 # Show a summary of the model. Check the number of trainable parameters
 model.summary()
 
-tf.config.run_functions_eagerly(True) # result won't be affected by eager/graph mode
+# tf.config.run_functions_eagerly(False) # result won't be affected by eager/graph mode
 # tf.data.experimental.enable_debug_mode()
 
 model.compile(loss=tf.losses.CategoricalCrossentropy(from_logits = True),
-              optimizer=optimizers.RMSprop(learning_rate=1e-3),
-              metrics=['acc'],
-              run_eagerly=True)
+              optimizer='adam',
+              metrics=['acc'])
 
 #%%
 # Train the model
 history = model.fit(
-      traingen,
-      epochs=20,
-      validation_data=valgen,
-      verbose=1)
+                    x=traingen, 
+                    batch_size=batch_size, 
+                    epochs=10, 
+                    verbose='auto',
+                    callbacks=None,
+                    validation_data=valgen, 
+                    shuffle=True,
+                    )
 
-# predictions = model.predict(x)
+
+predictions = model.predict(valgen.__getitem__(0)[0])
 
 
 
